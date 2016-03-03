a2315e9e04bd4aab03034a36a9d5ce0a2f60d02c
lstanish <lstanish@gmail.com[core]>
2016-03-03 15:31:44 -0700

    initial commit temperature lesson code

---
+++ _posts/OSIS-phenology-series/Temperature_Lesson.R
@@ -0,0 +1,9 @@
+#### Temperature Lesson Code ####
+# This lesson uses 30 minute temperature data from the triple aspirated
+# temperature sensors mounted above the canopy on the NEON tower.
+
+# To set the working directory on the Mac, you need to first mount
+# the remote drive onto your computer
+
+# Set working directory
+setwd("/Volumes/TOS/OSIS_dataLessons/temp_data/")
