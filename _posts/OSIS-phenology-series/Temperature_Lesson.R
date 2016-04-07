a56e2c50d55a2b840622b829e813ea6b8f4cf86e
lstanish <lstanish@gmail.com[core]>
2016-04-07 14:27:10 -0600

    Temp R code minor updates

--- _posts/OSIS-phenology-series/Temperature_Lesson.R
+++ _posts/OSIS-phenology-series/Temperature_Lesson.R
@@ -7,3 +7,24 @@
 
 # Set working directory
 setwd("/Volumes/TOS/OSIS_dataLessons/temp_data/")
+
+# Read in data
+harv.temp30 <- read.csv("NEON.D01.HARV.DP1.00003.001.00000.000.060.030.TAAT_30min.csv",
+                        stringsAsFactors = FALSE)
+scbi.temp30 <- read.csv("NEON.D02.SCBI.DP1.00003.001.00000.000.060.030.TAAT_30min.csv",
+                        stringsAsFactors = FALSE)
+
+## Format dateTime
+harv.temp30$startDateTime[1]
+
+## Convert the entire date/time values in format y-m-d h:m:s
+as.POSIXct(harv.temp30$startDateTime[2],format="%Y-%m-%dT%H:%M")
+new.date.time <- as.POSIXct(harv.temp30$startDateTime, format="%Y-%m-%dT%H:%M")
+
+# Convert to correct time zone, default for this code is MST
+# assign time zone to just the first entry
+harv.temp30$startDateTime <- as.POSIXct(harv.temp30$startDateTime,
+           format = "%Y-%m-%dT%H:%M", tz = "America/New_York")
+# check that conversion worked
+str(harv.temp30$startDateTime)
+
